50e4be6e9c292927b5a843e94dd3401a3471c208
JefersonLC <c.jeferson.lc@gmail.com>
2023-03-03 18:34:25 -0500

    user schema

---
+++ src/db/schemas/user.schema.js
@@ -0,0 +1,21 @@
+const Yup = require('yup');
+
+const name = Yup.string().max(30, 'Too Long').typeError('Must be a string');
+const lastname = Yup.string().max(30, 'Too Long').typeError('Must be a string');
+const age = Yup.number()
+  .integer('Must be an integer')
+  .typeError('Must be a number');
+const email = Yup.string().email('Invalid email').typeError('Must be a string');
+const phone = Yup.string()
+  .matches(/^9\d{8}/, 'Invalid phone number')
+  .typeError('Must be a string');
+
+const userSchema = Yup.object({
+  name: name.required('Name is required'),
+  lastname: lastname.required('Lastname is required'),
+  age: age.required('Age is required'),
+  email: email.required('Email is required'),
+  phone: phone.required('Phone is required'),
+});
+
+module.exports = { userSchema };
