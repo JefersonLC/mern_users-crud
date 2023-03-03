f1e3719557251d9bdb41ec65d5217b22d92f3563
JefersonLC <c.jeferson.lc@gmail.com>
2023-03-03 18:34:11 -0500

    user model

---
+++ src/db/models/user.model.js
@@ -0,0 +1,65 @@
+const { DataTypes } = require('sequelize');
+
+const User = [
+  'User',
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING(50),
+      allowNull: false,
+      set(value) {
+        this.setDataValue('name', value.trim().toLowerCase());
+      },
+      get() {
+        return this.getDataValue('name')
+          .toLowerCase()
+          .split(' ')
+          .map((e) => e[0].toUpperCase() + e.substring(1))
+          .join(' ');
+      },
+    },
+    lastname: {
+      type: DataTypes.STRING(50),
+      allowNull: false,
+      set(value) {
+        this.setDataValue('lastname', value.trim().toLowerCase());
+      },
+      get() {
+        return this.getDataValue('lastname')
+          .toLowerCase()
+          .split(' ')
+          .map((e) => e[0].toUpperCase() + e.substring(1))
+          .join(' ');
+      },
+    },
+    age: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING(50),
+      allowNull: false,
+      unique: true,
+      set(value) {
+        this.setDataValue('email', value.trim());
+      },
+    },
+    phone: {
+      type: DataTypes.STRING(50),
+      allowNull: false,
+      set(value) {
+        this.setDataValue('phone', value.trim());
+      },
+    },
+  },
+  {
+    tableName: 'users',
+    timestamps: false,
+  },
+];
+
+module.exports = User;
