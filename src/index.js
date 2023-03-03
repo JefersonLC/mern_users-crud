4e256e82a6fa63381c7052d4d20b8084fec330c8
JefersonLC <c.jeferson.lc@gmail.com>
2023-03-03 18:32:42 -0500

    entry point

---
+++ src/index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const UserController = require('./controllers/user.controller');
+const { userSchema } = require('./db/schemas/user.schema');
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log('Server on port ' + PORT);
+});
+
+app.use(express.json());
+
+// ROUTES
+app.get('/', UserController.find);
+
+app.post(
+  '/',
+  async (req, res, next) => {
+    try {
+      const data = req.body;
+      await userSchema.validate(data, { abortEarly: false });
+      next();
+    } catch (error) {
+      next(error);
+    }
+  },
+  UserController.create
+);
+
+app.use((error, req, res, next) => {
+  console.log(error);
+  res.json(error);
+});
