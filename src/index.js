b2cbd88a1620f22f1ba995f0885b3fa0bf341bd0
JefersonLC <c.jeferson.lc@gmail.com>
2023-03-06 21:13:00 -0500

    added new routes and error handling

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const cors = require('cors');
 const UserController = require('./controllers/user.controller');
-const { userSchema } = require('./db/schemas/user.schema');
+const { validator } = require('./middlewares/validator');
+const { addUserSchema, updateUserSchema } = require('./db/schemas/user.schema');
+const { EmptyResultError } = require('sequelize');
+const { ValidationError } = require('yup');
 
 const app = express();
 
@@ -11,27 +14,55 @@ app.listen(PORT, () => {
   console.log('Server on port ' + PORT);
 });
 
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 
 // ROUTES
-app.get('/', UserController.find);
 
-app.post(
-  '/',
-  async (req, res, next) => {
-    try {
-      const data = req.body;
-      await userSchema.validate(data, { abortEarly: false });
-      next();
-    } catch (error) {
-      next(error);
-    }
-  },
-  UserController.create
-);
+app
+  .route('/')
+  .get(UserController.find)
+  .post(validator({ schema: addUserSchema }), UserController.create);
 
+app
+  .route('/:id')
+  .get(UserController.findOne)
+  .patch(validator({ schema: updateUserSchema }), UserController.update)
+  .delete(UserController.remove);
+
+//ERROR HANDLING
 app.use((error, req, res, next) => {
   console.log(error);
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  if (error instanceof ValidationError) {
+    return res.status(400).json({
+      error: 'Bad Request',
+      status: 400,
+      message: error.message,
+      info: error.errors,
+      inner: error.inner.map((e) => ({
+        path: e.path,
+        errors: e.errors,
+      })),
+    });
+  }
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  if (error instanceof EmptyResultError) {
+    return res.status(404).json({
+      error: error.message,
+      status: 404,
+      message: "The user ID you entered doesn't exist",
+    });
+  }
+  next(error);
+});
+
+app.use((error, req, res, next) => {
   res.json(error);
 });
