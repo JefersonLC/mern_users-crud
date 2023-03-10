37b94e608034a69a2734cc29cdd68b5c661d0e19
JefersonLC <c.jeferson.lc@gmail.com>
2023-03-09 23:11:42 -0500

    Card component

---
+++ client/src/components/Card.jsx
@@ -0,0 +1,31 @@
+import Button from './Button'
+import useColor from '../hooks/useColor'
+
+export default function Card ({ user }) {
+  const { randomColor } = useColor(['#F00', '#0F0', '#00F'])
+
+  return (
+    <div className='card'>
+      <div className='card-header'>
+        <span className='avatar' style={{ backgroundColor: randomColor }}>
+          {user.name[0]}
+        </span>
+        <span className='names'>
+          {user.name} {user.lastname}
+        </span>
+        <span className='id'>#{user.id.toString().padStart(3, '0')}</span>
+      </div>
+      <div className='card-body'>
+        <ul className='list'>
+          <li className='item'>Phone: {user.phone}</li>
+          <li className='item'>Age: {user.age}</li>
+          <li className='item'>Email: {user.email}</li>
+        </ul>
+      </div>
+      <div>
+        <Button className='btn'>Modificar</Button>
+        <Button className='btn'>Eliminar</Button>
+      </div>
+    </div>
+  )
+}
