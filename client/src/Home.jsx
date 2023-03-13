84470b087745aebdac1d9ae76241a1a0773943a7
JefersonLC <c.jeferson.lc@gmail.com>
2023-03-12 21:05:44 -0500

    modified Home

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -1,14 +1,17 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
+import { useLoaderData } from 'react-router-dom'
 import Card from './components/Card'
 
 export default function Home () {
-  const [data, setData] = useState([])
+  const [data, setData] = useState(useLoaderData())
 
-  useEffect(() => {
-    fetch('http://localhost:3000/')
-      .then((res) => res.json())
-      .then((data) => setData(data))
-  }, [])
+  const deleteTask = id => {
+    fetch('http://localhost:3000/' + id, {
+      method: 'DELETE'
+    }).then(() => {
+      setData(data.filter((e) => e.id !== id))
+    })
+  }
 
   return (
     <>
@@ -16,9 +19,11 @@ export default function Home () {
         <input className='input' type='text' placeholder='Search user' />
       </section>
       <section className='cards'>
-        {data.map((user) => (
-          <Card key={user.id} user={user} />
-        ))}
+        {!data
+          ? (<div>Loading...</div>)
+          : data.length === 0
+            ? <div>Nothing</div>
+            : (data.map((user) => <Card key={user.id} user={user} remove={deleteTask} />))}
       </section>
     </>
   )
